6c2cd50cbfe219f36ced3993f5ccc81293822b0f
Samuel Štancl <samuel.stancl@gmail.com>
2021-05-27 15:45:58 +0200

    reorg

--- assets/views/components/meta.blade.php
+++ assets/views/components/meta.blade.php
@@ -1,14 +1,19 @@
-<meta property="og:type" content="website" />
-@if(seo('site')) <meta property="og:site_name" content="@seo('site')"> @endif
 @if(seo('title'))
     <title>@seo('title')</title>
     <meta property="og:title" content="@seo('title')" />
 @endif
+
 @if(seo('description')) 
     <meta property="og:description" content="@seo('description')" />
     <meta name="description" content="@seo('description')" />
 @endif
+
+<meta property="og:type" content="website" />
+
+@if(seo('site')) <meta property="og:site_name" content="@seo('site')"> @endif
+
 @if(seo('image')) <meta property="og:image" content="@seo('image')" /> @endif
+
 @if(seo('url'))
     <meta property="og:url" content="@seo('url')" />
     <link rel="canonical" href="@seo('url')" />
