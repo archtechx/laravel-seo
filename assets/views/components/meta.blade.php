103e49bc3fce525f505a3a0b5280aad17055884d
Lars Klopstra <larsklopstra@gmail.com>
2021-05-27 11:26:49 +0200

    Update meta.blade.php

--- assets/views/components/meta.blade.php
+++ assets/views/components/meta.blade.php
@@ -2,7 +2,10 @@
 <meta property="og:type" content="website" />
 @if(seo('site')) <meta property="og:site_name" content="@seo('site')"> @endif
 @if(seo('title')) <meta property="og:title" content="@seo('title')" /> @endif
-@if(seo('description')) <meta property="og:description" content="@seo('description')" /> @endif
+@if(seo('description')) 
+    <meta property="og:description" content="@seo('description')" />
+    <meta name="description" content="@seo('description')" />
+@endif
 @if(seo('image')) <meta property="og:image" content="@seo('image')" /> @endif
 @if(seo('url'))
     <meta property="og:url" content="@seo('url')" />
