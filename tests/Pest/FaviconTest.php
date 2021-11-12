72d148a0722e3231ad9b401da55ba225f9ec4fd1
Lars Klopstra <larsklopstra@gmail.com>
2021-11-12 15:30:50 +0100

    Update tests/Pest/FaviconTest.php
    Co-authored-by: Samuel Štancl <samuel.stancl@gmail.com>

--- tests/Pest/FaviconTest.php
+++ tests/Pest/FaviconTest.php
@@ -6,7 +6,7 @@ test("it should throw an exception if the given source icon doesn't exist", func
     seo()->favicon('i-dont-exist.png');
 })->throws(Exception::class, 'Given icon path `i-dont-exist.png` does not exist.');
 
-test('it should generate two favicons', function() {
+test('it should generate two favicons', function () {
     seo()->favicon(__DIR__ . '/../stubs/logo.png');
 
     assertFileExists(public_path('favicon.ico'));
