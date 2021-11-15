03c7fa5eee2bb2fd8b95df16342e87d9b6302c61
Lars Klopstra <larsklopstra@gmail.com>
2021-11-15 11:37:54 +0100

    wip

--- tests/Pest/FaviconTest.php
+++ tests/Pest/FaviconTest.php
@@ -1,14 +1,27 @@
 <?php
 
+use ArchTech\SEO\Commands\GenerateFaviconsCommand;
+
+use function Pest\Laravel\artisan;
 use function PHPUnit\Framework\assertFileExists;
 
-test("it should throw an exception if the given source icon doesn't exist", function () {
-    seo()->favicon('i-dont-exist.png');
-})->throws(Exception::class, 'Given icon path `i-dont-exist.png` does not exist.');
-
 test('it should generate two favicons', function () {
-    seo()->favicon(__DIR__ . '/../stubs/logo.png');
+    seo()->favicon();
+
+    $from = __DIR__ . '/../stubs/logo.png';
+
+    artisan(GenerateFaviconsCommand::class, [
+        'from' => $from,
+    ])->assertSuccessful();
 
     assertFileExists(public_path('favicon.ico'));
     assertFileExists(public_path('favicon.png'));
+});
+
+test('it should fail because the from path is incorrect', function () {
+    seo()->favicon();
+
+    artisan(GenerateFaviconsCommand::class, [
+        'from' => 'i/dont/exist.png',
+    ])->assertFailed();
 });
