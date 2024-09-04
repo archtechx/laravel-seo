cbb33fa269bb074f1737f8f9a0004f5ade984217
Ben Bjurstrom <benbjurstrom@users.noreply.github.com>
2024-09-04 09:54:37 -0700

    Add test

--- tests/Pest/ManagerTest.php
+++ tests/Pest/ManagerTest.php
@@ -1,6 +1,8 @@
 <?php
 
 use ArchTech\SEO\SEOManager;
+use Illuminate\Http\Request;
+use Mockery\MockInterface;
 
 test('set returns the set value', function () {
     expect(seo()->set('foo', 'bar'))->toBe('bar');
@@ -116,6 +118,16 @@ test('canonical url can be read from request', function () {
     expect(meta())
         ->toContain('<meta property="og:url" content="http://localhost" />')
         ->toContain('<link rel="canonical" href="http://localhost" />');
+});
+
+test('canonical url accepts origin', function () {
+    $this->get('/testing/5');
+
+    seo()->withUrl('https://foo.com');
+
+    expect(meta())
+        ->toContain('<meta property="og:url" content="https://foo.com/testing/5" />')
+        ->toContain('<link rel="canonical" href="https://foo.com/testing/5" />');
 });
 
 test('canonical url can be changed', function () {
